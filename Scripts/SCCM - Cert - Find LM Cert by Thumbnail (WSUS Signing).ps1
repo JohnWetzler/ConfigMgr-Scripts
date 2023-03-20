bc3b9ac33c65ff67195b95bab22802e7d25f93f6
JohnWetzler <126033832+JohnWetzler@users.noreply.github.com>
2023-03-20 12:57:00 -0500

    Add files via upload

---
+++ Scripts/SCCM - Cert - Find LM Cert by Thumbnail (WSUS Signing).ps1
@@ -0,0 +1,15 @@
+﻿<#if (Get-ChildItem -Path Cert:\LocalMachine\Root | Where-Object {$_.Thumbprint -eq "0edef690226f7a527afbfb8682b0387cc2a7fba9"})
+{Write-Host "Installed"}
+#>
+
+function DetectCert {
+$ThumbPrint = "0edef690226f7a527afbfb8682b0387cc2a7fba9"
+$FindCert = (Get-ChildItem -Path Cert:\LocalMachine\Root | Where-Object {$_.Thumbprint -eq $ThumbPrint})
+	If ([boolean]$FindCert -eq $true) {
+		Return $True
+	} else {
+		Return $False
+	}
+}
+
+DetectCert
